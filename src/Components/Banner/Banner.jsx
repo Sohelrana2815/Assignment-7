06d0af87d25a8ba6149b3bcf06c8e57bda31a242
Sohelrana2815 <sohel152302@gmail.com>
2024-07-17 13:13:26 +0600

    banner img gradient fix

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,16 +4,21 @@ const Banner = () => {
     <>
       <div className="relative ">
         <img src={banner} alt="Chef photo" />
+        <div className="absolute inset-0 bg-gradient-to-r from-black to-gray-900 opacity-60"></div>
+
         <div className="absolute inset-0 text-center lg:top-1/3 ">
           <div>
             <div className="text-white lg:w-[795px] lg:mx-auto lg:space-y-6 mt-12 ">
               <h1 className="lg:text-5xl lg:leading-normal">
                 Discover an exceptional cooking class tailored for you!
               </h1>
-              <p className="lg:text-lg lg:leading-loose">
-                Learn and Master Basic Programming, Data Structures, Algorithm,
-                OOP, Database and solve 500+ coding problems to become an
-                exceptionally well world-class Programmer.
+              <p className="lg:text-xl font-normal  lg:leading-loose ">
+                Welcome to your ultimate culinary destination! Here, you will
+                discover a treasure trove of delicious recipes that cater to all
+                tastes and skill levels. Whether you’re a seasoned chef or just
+                starting your cooking journey, our collection is designed to
+                inspire and delight. Let’s embark on this flavorful adventure
+                together!
               </p>
             </div>
             <div className="lg:mt-10">
